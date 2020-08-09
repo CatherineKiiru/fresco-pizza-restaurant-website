45577ed8ddf928a7174e47b68f57ed4e5d7e663f
Catherine <wambukukiiru@gmail.com>
2020-08-10 01:36:01 +0300

    updated JS script

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
-$(document).ready(function(){
-
-let theForm = document.form['pizzaform'];
-let prices = new Array();
-prices["small"]=800;
-prices["medium"]=1000;
-prices["small"]=1200;
-prices["crust1"]=200;
-prices["crust2"]=250;
-prices["crust3"]=300;
-prices["pepperoni"]=200;
-prices["cheese"]=250;
-prices["vegan"]=300;
-
-function getPizzaPrices(){
-    let pizzaPrices=0;
-    let theForm = document.form['pizzaform'];
-    let pizza = theForm.elements['pizza'];
-    for(var i=0; i<pizza.length; i++);
-    if(pizza[i].checked);
-
-    pizzaPrices = prices[pizza[i].value];
-        break;
-        alert (pizzaPrices)
+window.onload=function(){
+    var inputs = document.getElementsByClassName('sum'),
+        total  = document.getElementById('payment-total');
+    
+     for (var i=0; i < inputs.length; i++) {
+        inputs[i].onchange = function() {
+            var add = this.value * (this.checked ? 1 : -1);
+            total.innerHTML = parseFloat(total.innerHTML) + add
+            var new_total = parseFloat(document.getElementById('input').value);
+          console.log(new_total);
+            document.getElementById('input').value=new_total + add
+        }
+      }
     }
-});
-
